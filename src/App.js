277c8ea1dd492132005bf8d95a94143c36e7b9c7
Sukant Arora <sukant.iitr@gmail.com>
2021-01-03 23:51:34 +0530

    Add search input box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SearchBox from "./components/SearchBox";
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <h1>Weather App</h1>
       </header>
       <main>
-
+        <SearchBox />
       </main>   
       <footer className="App__Footer">
         <p className="Copyright">&#169; 2020 Sukant Arora. All Rights Reserved.</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,20 +1,65 @@
-import React from "react";
-import searchSymbol from "../assets/search.svg";
+import React, { useState } from "react";
+import MagnifierSymbol from "../assets/search.svg";
 
-const handleSubmit = (event) => {
-  console.log(event.target.value);
-}
+
 
 const SearchBox = () => {
+  
+  const [cityName, setCityName] = useState("");
+
+  const handleSubmit = (event) => {
+    console.log(`${cityName}'s weather is searched.`);
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    setCityName(event.target.value);
+  };
+
   return (
-    <div className="SearchBox">
+    <form className="SearchBox" onSubmit={handleSubmit}>
+      <span className="SearchBox__MagnifierSpan">
+          <img 
+            src={MagnifierSymbol}
+            className="SearchBox__MagnifierSymbol"
+            alt="magnifier symbol" />
+      </span>
       <input 
         type="text"
-        onSubmit={handleSubmit}
-        placeholder="Enter city name"
+        value={cityName}
+        placeholder="Enter city"
+        className="SearchBox__Input"
+        onChange={handleChange}
       />
-    </div>
+    </form>
   )
 };
 
 export default SearchBox;
+
+// function SearchBox1(){
+//   return (
+//       <main className="search-main">
+//           <div className="search-form-div">
+//               <div className="search-box">
+//                   <div className="magnifier-div">
+//                       <div className="magnifier-wrapper">
+//                           <span className="magnifier-symbol">
+//                               <img src={MagnifierSymbol} alt="magnifier symbol" />
+//                           </span>
+//                       </div>
+//                   </div>
+//                   <div className="input-div">
+//                       <input type="search" className="search-input-area" />
+//                   </div>
+//               </div>
+//           </div>
+//           <div className="search-buttons-wrapper">
+//               <div className="search-buttons">
+//                   <button type="button" id="mdi-search">MDI Search</button>
+//                   <button type="button" id="feeling-lucky">I'm Feeling Lucky</button>
+//               </div>
+//           </div>
+//       </main>
+//   )
+// }
